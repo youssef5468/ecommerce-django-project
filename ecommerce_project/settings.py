d5466babed744a87ae3a791b1f8584415073c6d3
youssef <yousefkhamisyahoo@gmail.com>
2026-08-26 02:23:23 +0200

    Fix whitenoise static files storage error

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -132,6 +132,6 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
